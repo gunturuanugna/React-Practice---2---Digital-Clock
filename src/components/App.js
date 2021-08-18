d3536db6474960bd76f5cba7fad9e4c76a637289
gunturuanugna <anugnagunturu1@gmail.com>
2021-08-18 20:45:38 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,33 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time: getCurrentTime(),
+        };
+    }
+
+    componentDidMount(){
+        this.intervalId = setInterval( ()=>{
+            this.setState({time:getCurrentTime() })
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId)
+    }
+    
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+                <h3 id="time">{this.state.time}</h3>
+            </div>
         )
     }
 }
+
 
 
 export default App;
